bddef4d8745c79af221aa79fb0b1ccddddd44fa1
giraffekey <giraffekey@tutanota.com>
2023-01-08 02:01:14 -0800

    Negative coordinates

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,8 @@ const colors = [
   0xff0000, 0x00ff00, 0x0000ff, 0xffff00, 0xff00ff, 0x00ffff, 0xffffff,
   0x000000,
 ]
+const minCoord = -2147483648
+const maxCoord = 2147483647
 
 const pixels: number[][] = []
 const pos = { x: 0, y: 0 }
@@ -28,20 +30,20 @@ let size = window.innerWidth / width
 let height = window.innerHeight / size
 let speed = width / 4
 
-for (let i = 0; i < 1_000; i++) {
-  pixels[i] = []
-  for (let j = 0; j < 1_000; j++) {
-    pixels[i][j] = colors[Math.floor(Math.random() * colors.length)]
+for (let i = 0; i < 200; i++) {
+  pixels[i - minCoord - 100] = []
+  for (let j = 0; j < 200; j++) {
+    pixels[i - minCoord - 100][j - minCoord - 100] = colors[Math.floor(Math.random() * colors.length)]
   }
 }
 
 function drawCanvas() {
   for (let j = 0; j < width + 1; j++) {
     for (let i = 0; i < height + 1; i++) {
-      graphics.beginFill(pixels[Math.floor(pos.x) + j][Math.floor(pos.y) + i])
+      graphics.beginFill(pixels[Math.floor(pos.x) - minCoord + j][Math.floor(pos.y) - minCoord + i])
       graphics.drawRect(
-        (j - (pos.x % 1.0)) * size,
-        (i - (pos.y % 1.0)) * size,
+        (j - (pos.x - Math.floor(pos.x))) * size,
+        (i - (pos.y - Math.floor(pos.y))) * size,
         size,
         size,
       )
@@ -57,11 +59,11 @@ app.stage.addChild(graphics)
 function move(dx: number, dy: number) {
   if (dx != 0 || dy != 0) {
     pos.x += dx
-    if (pos.x < 0) pos.x = 0
-    if (pos.x > 999 - width) pos.x = 999 - width
+    if (pos.x < minCoord) pos.x = minCoord
+    if (pos.x > maxCoord - width) pos.x = maxCoord - width
     pos.y += dy
-    if (pos.y < 0) pos.y = 0
-    if (pos.y > 999 - height) pos.y = 999 - height
+    if (pos.y < minCoord) pos.y = minCoord
+    if (pos.y > maxCoord - height) pos.y = maxCoord - height
     $position.innerText = `(${Math.floor(pos.x)}, ${Math.floor(pos.y)})`
     graphics.clear()
     drawCanvas()
