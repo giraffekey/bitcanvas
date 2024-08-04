f6fc803109117a8714b603d019a444ea72838279
giraffekey <giraffekey@tutanota.com>
2024-08-04 09:20:41 -0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const isProduction = process.env.NODE_ENV == "production"
 const config = {
   entry: "./src/index.ts",
   output: {
-    path: path.resolve(__dirname, "build"),
+    path: __dirname,
   },
   devServer: {
     open: true,
